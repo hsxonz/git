1b0781f8b24f841fd3a1378ee170c664b9248dff
hsxonz <heroallstart2001@gmail.com>
2025-08-13 18:57:37 +0700

    update

--- index.py
+++ index.py
@@ -1,10 +1,22 @@
 import requests
 
-url = "https://proxyproxy.online/chuyen_mon/cau_hoi"
-payload = {
-    "cau_hoi": "Viết hàm kiểm tra số nguyên n có phải là số nguyên tố hay không. Biết rằng số nguyên tố là số \u003E 1, chỉ chia hết cho 1 và chính nó. Hàm trả về 1 nếu n là số nguyên tố, ngược lại trả về 0. Ngôn ngữ có thể dùng: C/C  , C"
-}
+def get_cau_hoi():
+    url = "https://proxyproxy.online/chuyen_mon/cau_hoi"
+    payload = {
+        "cau_hoi": "Viết hàm kiểm tra số nguyên n có phải là số nguyên tố hay không. Biết rằng số nguyên tố là số \u003E 1, chỉ chia hết cho 1 và chính nó. Hàm trả về 1 nếu n là số nguyên tố, ngược lại trả về 0. Ngôn ngữ có thể dùng: C/C  , C"
+    }
 
-response = requests.post(url, json=payload)  # Dùng POST và gửi JSON
-data = response.json()
-print(data)
+    response = requests.post(url, json=payload)  # Dùng POST và gửi JSON
+    data = response.json()
+    print(data)
+
+
+def get_dap_an():
+    url = "https://proxyproxy.online/chuyen_mon/dap_an"
+
+    response = requests.get(url)  # Dùng POST và gửi JSON
+    data = response.json()
+    print(data)
+
+# get_cau_hoi()
+get_dap_an()
