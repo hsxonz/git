90f4951ab097fb12325511977c3a12e13a79912c
hsxonz <heroallstart2001@gmail.com>
2025-08-14 13:32:19 +0700

    updae

--- index.py
+++ index.py
@@ -6,7 +6,7 @@ def get_cau_hoi():
         "cau_hoi": "Viết hàm kiểm tra số nguyên n có phải là số nguyên tố hay không. Biết rằng số nguyên tố là số \u003E 1, chỉ chia hết cho 1 và chính nó. Hàm trả về 1 nếu n là số nguyên tố, ngược lại trả về 0. Ngôn ngữ có thể dùng: C/C  , C"
     }
 
-    response = requests.post(url, json=payload)  # Dùng POST và gửi JSON
+    response = requests.post(url, json=payload) 
     data = response.json()
     print(data)
 
@@ -14,7 +14,7 @@ def get_cau_hoi():
 def get_dap_an():
     url = "https://proxyproxy.online/chuyen_mon/dap_an"
 
-    response = requests.get(url)  # Dùng POST và gửi JSON
+    response = requests.get(url) 
     data = response.json()
     print(data)
 
